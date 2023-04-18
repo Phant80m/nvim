e81b436938f911a4cb8e49ffd866638baaa37242
Nicholas Samuels <70614900+Phant80m@users.noreply.github.com>
2023-04-18 23:21:51 +1000

    fix some directory issues

--- install.sh
+++ install.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
- git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim 
+if [ -d ~/.local/share/nvim/site/pack/packer/start/packer.nvim ]; then
+  read -p "$(tput setaf 1)The Packer directory already exists. Do you want to override it? (y/n)$(tput sgr0) " -n 1 -r
+  echo
+  if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    exit 1
+  fi
+  rm -rf ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+fi
 
- git clone https://github.com/Phant80m/nvim ~/.config/nvim/
+if [ -d ~/.config/nvim/ ]; then
+  read -p "$(tput setaf 1)The Neovim configuration directory already exists. Do you want to override it? (y/n)$(tput sgr0) " -n 1 -r
+  echo
+  if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    exit 1
+  fi
+  rm -rf ~/.config/nvim/
+fi
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+git clone https://github.com/Phant80m/nvim ~/.config/nvim/
